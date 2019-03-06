eaa1f1618100326c199975e21039e3c48c91370e
kriti-garg <kritigarg1998@gmail.com>
2019-03-06 17:42:29 +0530

    modify models.py

--- QuizApp/quiz/models.py
+++ QuizApp/quiz/models.py
@@ -3,23 +3,9 @@ from django.db import models
 class genres(models.Model):
     content = models.TextField();
 
-class leaderboards(models.Model):
-    score = models.IntegerField(default=0)
-    user_id = models.ForeignKey(users, on_delete=models.CASCADE)
-    subgenre_id = models.ForeignKey(subgenre, on_delete=models.CASCADE)
-
 class subgenre(models.Model):
     cont = models.TextField()
-    genre_id = models.ForeignKey(genre, on_delete=models.CASCADE)
-
-class questions(models.Model):
-    ques = models.CharFiels(max_length=200)
-    optA = models.TextField()
-    optB = models.TextField()
-    optC = models.TextField()
-    optD = models.TextField()
-    correctopt = models.TextField()
-    subgenre_id = models.ForeignKey(subgenre, on_delete=models.CASCADE)
+    genre_id = models.ForeignKey(genres, on_delete=models.CASCADE)
 
 class users(models.Model):
     name = models.TextField()
@@ -28,10 +14,25 @@ class users(models.Model):
     remember_digest = models.TextField()
     admin = models.BooleanField(default=0)
 
+class leaderboards(models.Model):
+    score = models.IntegerField(default=0)
+    user_id = models.ForeignKey(users, on_delete=models.CASCADE)
+    subgenre_id = models.ForeignKey(subgenre, on_delete=models.CASCADE)
+
+class questions(models.Model):
+    ques = models.CharField(max_length=200)
+    optA = models.TextField()
+    optB = models.TextField()
+    optC = models.TextField()
+    optD = models.TextField()
+    correctopt = models.TextField()
+    subgenre_id = models.ForeignKey(subgenre, on_delete=models.CASCADE)
+
+
 class states(models.Model):
     subgenre_id = models.ForeignKey(subgenre, on_delete=models.CASCADE)
     users_id = models.ForeignKey(users, on_delete=models.CASCADE)
-    qno = IntegerField(default=0)
-    score = IntegerField(defalut=0)
+    qno = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
 
 
